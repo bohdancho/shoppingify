772c764d6759adf54ea12a210a0504829693754b
Bohdan Chornokondratenko <46049032+bohdancho@users.noreply.github.com>
2023-12-25 11:13:21 +0100

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,36 @@
+/** @type {import("eslint").Linter.Config} */
+const config = {
+  root: true,
+  env: { browser: true, es2020: true, node: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', 'node_modules', '!.*.*'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: { project: true },
+  plugins: ['react-refresh', '@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/array-type': 'off',
+    '@typescript-eslint/consistent-type-definitions': 'off',
+    '@typescript-eslint/prefer-nullish-coalescing': 'off',
+    '@typescript-eslint/consistent-type-imports': [
+      'warn',
+      {
+        prefer: 'type-imports',
+        fixStyle: 'inline-type-imports',
+      },
+    ],
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-misused-promises': [
+      2,
+      {
+        checksVoidReturn: { attributes: false },
+      },
+    ],
+  },
+}
+
+module.exports = config
---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css'
+
+export function App() {
+  return (
+    <div className='h-screen w-screen bg-slate-800 text-center text-white'>
+      <h1>Vite + React</h1>
+      <div className='card'>it works!</div>
+    </div>
+  )
+}
