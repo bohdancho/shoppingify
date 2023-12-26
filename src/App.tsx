7ed0e5f1c45c4d32f4ae1d59414a452784b74cc7
bohdancho <bogdan.chornocondratenko@gmail.com>
2023-12-26 13:07:00 +0100

    Revert "Evolu deploy PoC"
    This reverts commit b243de52b994684c5fe06cacbefc71239a7f0d5d.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,7 @@
 import { RouterProvider } from '@tanstack/react-router'
 import './App.css'
 import { router } from './router'
-import { Evolu } from './db/evolu'
 
 export function App() {
-  return (
-    <>
-      <Evolu />
-      <RouterProvider router={router} />
-    </>
-  )
+  return <RouterProvider router={router} />
 }
--- vite.config.ts
+++ vite.config.ts
@@ -5,52 +5,14 @@ import topLevelAwait from 'vite-plugin-top-level-await'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  optimizeDeps: {
-    // A workaround for Vite bug: https://github.com/vitejs/vite/issues/13314#issuecomment-1560745780
-    exclude: ['@evolu/react'],
-    // Another workaround for Vite bug: https://github.com/radix-ui/primitives/discussions/1915#discussioncomment-5733178
-    include: ['react-dom'],
-  },
   base: './',
   server: {
     port: 3000,
   },
-  plugins: [
-    react(),
-    topLevelAwait(),
-    {
-      configureServer(server) {
-        server.middlewares.use((_req, res, next) => {
-          // https://sqlite.org/wasm/doc/trunk/persistence.md#coop-coep
-          res.setHeader('Cross-Origin-Opener-Policy', 'same-origin')
-          res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp')
-          next()
-        })
-      },
-      name: 'configure-server',
-    },
-  ],
+  plugins: [react(), topLevelAwait()],
   resolve: {
     alias: {
       '~': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
-  worker: {
-    format: 'es',
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: `[name][hash].js`,
-        chunkFileNames: `[name][hash].js`,
-        assetFileNames: `[name][hash].[ext]`,
-      },
-    },
-  },
-  preview: {
-    headers: {
-      'Cross-Origin-Opener-Policy': 'same-origin',
-      'Cross-Origin-Embedder-Policy': 'require-corp',
     },
   },
 })
